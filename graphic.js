61df60bbaecafe6140c9f6f49234e2a963c6944b
Chau Nguyen <cn490@georgetown.edu>
2021-12-08 18:52:19 -0500

    DC map can change color on scroll

---
+++ graphic.js
@@ -0,0 +1,276 @@
+let dataset
+
+const margin = {left: 0, top: 50, bottom: 50, right: 50}
+const width = 800 - margin.left - margin.right
+const height = 800 - margin.top - margin.bottom
+//svg
+    let svg = d3.select("svg");
+
+    //svg width and height
+    svg.attr('width',500)
+        .attr('height',500)
+
+
+    //set up grid spacing
+    let spacing = 40;
+    let rows = 10;
+    let column = 10;
+    let randnum = (min,max) => Math.round( Math.random() * (max-min) + min );
+    
+    let projection = d3.geoMercator();
+    let path = d3.geoPath().projection(projection);
+
+    d3.json("GeoJSON/Neighborhood_Clusters.geojson").then(function(d){
+        dataset = d
+        console.log(dataset)
+        grid()
+    });
+    
+    function grid(){
+        projection.fitSize([width/2,height/2],dataset);
+        svg.selectAll("path")
+            .data(dataset.features)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .style("fill", "navy")
+            .style("stroke", "yellow")
+    }
+
+    let grid2 = () =>{
+        projection.fitSize([width/2,height/2],dataset);
+        svg.selectAll("path")
+            // .data(dataset.features)
+            // .enter()
+            // .append("path")
+            // .attr("d", path)
+            .transition()
+                .duration(500)
+                .delay(1000)
+            .attr("d", path)
+            .style("fill", "red")
+            .style("stroke", "yellow")
+    }
+
+// THIS WORKS
+    // d3.json("GeoJSON/Neighborhood_Clusters.geojson").then(function(d){
+    //     dataset = d
+    //     console.log(dataset)
+    //     drawInitial()
+    // });
+
+    // function drawInitial(){
+    //     projection.fitSize([width/2,height/2],dataset);
+    //     svg.selectAll("path")
+    //         .data(dataset.features)
+    //         .enter()
+    //         .append("path")
+    //         .attr("d", path)
+    //         .style("fill", "navy")
+    //         .style("stroke", "yellow")
+    //}
+    // function updateMap(){
+    //     //Update all rects
+    //     svg.selectAll("path")
+    //       .data(dataset.features)
+    //       .transition() // <---- Here is the transition
+    //       .duration(2000)
+    //       .append("path")
+    //       .attr("d", path)
+    //       .style("fill", "black")
+    //       .style("stroke", "blue");
+
+    // //Create an array of objects
+    // let our_data = d3.range(20).map(i => 
+    // ({ 'device': i < 5 ? 'ios' : 'android', 
+    //     'city': i < 3 ? 'San Diegan' : 'Out of towners', 
+    //     'age': randnum(25, 65)
+    // }));
+
+
+    // //create group and join our data to that group
+    // let group = svg.selectAll('g')
+    //   .data(our_data)
+    //   .enter()
+    //   .append("g")
+
+
+
+    // //create rectangles
+    // let rects = group.append("rect")
+
+
+    // //city data
+    // d3.selectAll('g')
+    //   .append("text")
+    //   .text( (d) => d["city"])
+    //   .attr("fill", "black")
+    //   .attr("class", "city")
+    //   .attr("dx", -500)
+
+
+
+    // //age data
+    // d3.selectAll('g')
+    //   .append("text")
+    //   .text( (d) => d["age"] )
+    //   .attr("fill", "#fff")
+    //   .attr("class", "age")
+    //   .attr("dx", -500)
+
+
+
+
+
+
+
+
+    // square grid
+    // let grid = () =>{
+    //   rects
+    //     .transition()
+    //     .delay((d, i) => 10 * i)
+    //     .duration(600)
+    //     .ease("elastic")
+    //     .attr("width", 20)
+    //     .attr("height", 20)
+    //     .attr("rx", 5)
+    //     .attr("ry", 5)
+    //     .attr("x", (d, i) => i % column * spacing)
+    //     .attr("y", (d, i) => Math.floor(i / 10) % rows * spacing)
+    //     .attr("fill", (d, i) => (i < 40 ? "black" : "red"))
+    //     .attr("opacity", "1")
+    // }
+
+
+
+    // //circle grid
+    // let grid2 = () =>{
+    //   rects
+    //     .transition()
+    //     .delay((d, i) => 10 * i)
+    //     .duration(600)
+    //     .ease("elastic")
+    //     .attr("width", 20)
+    //     .attr("height", 20)
+    //     .attr("rx", "50%")
+    //     .attr("ry", "50%")
+    //     .attr("x", (d, i) => i % column * spacing)
+    //     .attr("y", (d, i) => Math.floor(i / 10) % rows * spacing)
+    //     .attr("fill", (d, i) => (i < 8 ? "none" : "white"))
+    // }
+
+
+    // //divide
+    // let divide = () =>{
+    //   rects
+    //     .transition()
+    //     .delay((d, i) => 10 * i)
+    //     .duration(600)
+    //     .ease("elastic")
+    //     .attr("width", 10)
+    //     .attr("height", 10)
+    //     .attr("rx", 0)
+    //     .attr("ry", 0)
+    //     .attr("x", (d, i) => d['device'] == "ios" ? randnum(100, 150) :  randnum(300, 350))
+    //     .attr("y", (d, i) => i * 20)
+    //     .attr("fill", (d, i) => d['device'] == "ios" ? "#394147": "#99c125")
+    //     .attr("opacity", (d,i)=> i < 12 ? 1 : 0 )//only show 12 people
+
+
+
+    //     //age
+    //     d3.selectAll('text.age')
+    //       .transition()
+    //       .delay( (d,i) => 40*i ) 
+    //       .duration(900)
+    //       .ease('elastic')
+    //       .attr("dx", -500)
+
+
+    //     //city
+    //     d3.selectAll('text.city')
+    //       .transition()
+    //       .delay( (d,i) => 40*i ) 
+    //       .duration(900)
+    //       .ease('elastic')
+    //       .attr("dx", -500)
+    // }
+
+
+
+
+
+    // //bar cart
+    // let barChart = () => {
+    //   rects
+    //     .attr("rx", 0 )
+    //     .attr("ry", 0 )
+    //     .transition()
+    //     .delay( (d,i) => 20*i ) 
+    //     .duration(900)
+    //     .ease('elastic')//linear, quad, cubic, sin, exp, circle, elastic, back, bounce
+    //     .attr("x", (d,i) => 150 )
+    //     .attr("y", (d,i) => i * 17 )
+    //     .attr("width", (d,i) => d["age"])
+    //     .attr("height", (d,i) => 15)
+    //     .attr("fill", (d, i) => (i < 3 ? "#99c125" : "#394147"))
+    //     .attr("opacity", 1)
+    //     .attr("transform", "translate(0,0) rotate(0)")
+    //     .attr("opacity", (d,i)=> i < 12 ? 1 : 0 )//only show 12 people
+
+
+    //   //age text
+    //   d3.selectAll('text.age')
+    //     .transition()
+    //     .delay( (d,i) => 20*i ) 
+    //     .duration(900)
+    //     .ease('elastic')
+    //     //align text right
+    //     .attr("text-anchor", "start")
+    //     .attr("dx", 160)
+    //     .attr("dy", (d,i)=> (i * 17) + 12)
+    //     .attr("opacity", (d,i)=> i < 12 ? 1 : 0 )//nly show 12 people
+
+
+
+    //   //city text
+    //   d3.selectAll('text.city')
+    //     .transition()
+    //     .delay( (d,i) => 20*i ) 
+    //     .duration(900)
+    //     .ease('elastic')
+    //     //align text left
+    //     .attr("text-anchor", "end")
+    //     .attr("dx", 140)
+    //     .attr("dy", (d,i)=> (i * 17) + 12)
+    //     .attr("opacity", (d,i)=> i < 12 ? 1 : 0 )//only show 12 people
+
+
+    // }
+
+
+
+
+    // //waypoints scroll constructor
+    function scroll(n, offset, func1, func2){
+      return new Waypoint({
+        element: document.getElementById(n),
+        handler: function(direction) {
+           direction == 'down' ? func1() : func2();
+        },
+        //start 75% from the top of the div
+        offset: offset
+      });
+    };
+
+
+
+    //triger these functions on page scroll
+    new scroll('div2', '75%', grid2, grid);
+    // new scroll('div4', '75%', divide, grid);
+    // new scroll('div6', '75%', barChart, divide);
+
+
+
+    //start grid on page load
--- scroller.js
+++ scroller.js
@@ -1,47 +1,25 @@
-/**
- * Scroller
- * This function handles the identifies which section the user is currently scrolled on
- */
-
- function scroller(){
+function scroller(){
     let container = d3.select('body')
-    // Event dispatcher
     let dispatch = d3.dispatch('active', 'progress');
-    // D3 selection of text sections that will be scrolled through
     let sections = d3.selectAll('.step')
-    // Empty array that will hold the y coordinate of each section that will be scrolled through
     let sectionPositions
    
     let currentIndex = -1
-    // Initial y coordinate
     let containerStart = 0;
 
-    /**
-     * Scroll - constructor function.
-     * Sets up scroller to monitor scrolling of text sections
-     */
     function scroll(){
-        // When window is scrolled, call position function
-        // When window is resized, call resize function
-            // The resize function is important for building a dynamic web page that adapts to screen resolution
         d3.select(window)
             .on('scroll.scroller', position)
             .on('resize.scroller', resize)
 
-        // Manually call resize to initially set up scroller
         resize();
 
-        // 'Hack' to get position to be called once for scroll position on load
         let timer = d3.timer(function() {
             position();
             timer.stop();
         });
     }
 
-    /**
-     * Resize - called initially and when page is resized.
-     * Resets the sectionPositions
-     */
     function resize(){
         sectionPositions = [];
         let startPos;
@@ -56,10 +34,6 @@
         });
     }
 
-    /**
-     * Position - get current users position
-     * If user has scrolled to new section, dispatch active event with new section index
-     */
     function position() {
         let pos = window.pageYOffset - 300 - containerStart;
         let sectionIndex = d3.bisect(sectionPositions, pos);
@@ -76,11 +50,6 @@
         dispatch.call('progress', this, currentIndex, progress)
     }
 
-    /**
-     * Container - get/set the parent element of the sections
-     * Useful for if the scrolling doesn't start at the very top of the page
-     * For example, when the page is refreshed but doesn't reset at the top of the page
-     */
     scroll.container = function(value) {
         if (arguments.legth === 0){
             return container
@@ -95,4 +64,3 @@
 
     return scroll;
 }
-
